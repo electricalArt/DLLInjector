b8bc82197e60195a1d2aaa3b8d9a7f2062026916
artur <muslimow9@yandex.ru>
2024-07-21 18:41:42 +0300

    On branch master 	modified:   DLLInjector32/Source.cpp

--- DLLInjector32/Source.cpp
+++ DLLInjector32/Source.cpp
@@ -2,7 +2,7 @@
 #include <tlhelp32.h>
 #include <stdio.h>
 
-constexpr size_t GAME_NAME_LENGTH_MAX = 500;
+constexpr size_t PROCESS_NAME_LENGTH_MAX = 500;
 
 void ExitError(const char* szcMsg) {
     puts(szcMsg);
@@ -10,8 +10,8 @@ void ExitError(const char* szcMsg) {
     exit(1);
 }
 
-//  Finds the game process. Returns found game process handle.
-HANDLE GetProcessHandle(const wchar_t* szcGameName) {
+//  Finds the process. Returns found process handle.
+HANDLE GetProcessHandle(const wchar_t* szcProcessName) {
     DWORD wProcessId = 0;
     HANDLE hSnapshot = 0;
     PROCESSENTRY32 processEntry = { 0 };
@@ -20,7 +20,7 @@ HANDLE GetProcessHandle(const wchar_t* szcGameName) {
     hSnapshot = CreateToolhelp32Snapshot(TH32CS_SNAPPROCESS, NULL);
     Process32First(hSnapshot, &processEntry);
     do {
-        if (wcscmp(processEntry.szExeFile, szcGameName) == 0) {
+        if (wcscmp(processEntry.szExeFile, szcProcessName) == 0) {
             wProcessId = processEntry.th32ProcessID;
             break;
         }
@@ -31,31 +31,31 @@ HANDLE GetProcessHandle(const wchar_t* szcGameName) {
     return hProcess;
 }
 
-//  Writes string that contains hack .dll path to the game process. Stores written memory
-//  address to `lpProcessSzcDllPath` variable.
-LPVOID WriteHackDllToProcess(HANDLE hProcess, const wchar_t* szcDllPath) {
-    LPVOID lpProcessSzcDllPath = VirtualAllocEx(
+//  Writes string that contains hack .dll path to the process. Stores written memory
+//  address to `lpProcessSzcDLLPath` variable.
+LPVOID WriteHackDLLToProcess(HANDLE hProcess, const wchar_t* szcDLLPath) {
+    LPVOID lpProcessSzcDLLPath = VirtualAllocEx(
         hProcess,
         NULL,
-        (wcslen(szcDllPath) + 1) * sizeof(wchar_t),
+        (wcslen(szcDLLPath) + 1) * sizeof(wchar_t),
         MEM_COMMIT,
         PAGE_READWRITE );
-    if (lpProcessSzcDllPath == NULL) {
-        ExitError("ERROR: lpProcessSzcDllPath is NULL");
+    if (lpProcessSzcDLLPath == NULL) {
+        ExitError("ERROR: lpProcessSzcDLLPath is NULL");
     }
     SIZE_T cbWritten = 0;
     WriteProcessMemory(
         hProcess,
-        lpProcessSzcDllPath,
-        szcDllPath,
-        (wcslen(szcDllPath) + 1) * sizeof(wchar_t),
+        lpProcessSzcDLLPath,
+        szcDLLPath,
+        (wcslen(szcDLLPath) + 1) * sizeof(wchar_t),
         &cbWritten);
 
-    return lpProcessSzcDllPath;
+    return lpProcessSzcDLLPath;
 }
 
-//  Creates remote thread in the game process. This thread starts hack .dll.
-HANDLE CreateHackThread(HANDLE hProcess, LPVOID lpProcessSzcDllPath) {
+//  Creates remote thread in the process. This thread starts hack .dll.
+HANDLE CreateHackThread(HANDLE hProcess, LPVOID lpProcessSzcDLLPath) {
     HMODULE hKernel32base = GetModuleHandleA("kernel32.dll");
     if (hKernel32base == NULL) {
         ExitError("hKernel32base is NULL");
@@ -67,7 +67,7 @@ HANDLE CreateHackThread(HANDLE hProcess, LPVOID lpProcessSzcDllPath) {
         NULL,
         0,
         lpThreadStartAddress,
-        lpProcessSzcDllPath,
+        lpProcessSzcDLLPath,
         0,
         &wThreadId);
     if (hThread == NULL) {
@@ -91,23 +91,23 @@ void WaitThread(HANDLE hThread) {
 
 int wmain(int argc, wchar_t* argv[]) {
     if (argv[1] == NULL) {
-        ExitError("ERROR: GameName is not specified");
+        ExitError("ERROR: ProcessName is not specified");
     }
     if (argv[2] == NULL) {
-        ExitError("ERROR: DllPath is not specified");
+        ExitError("ERROR: DLLPath is not specified");
     }
 
-    const wchar_t* szcGameName = argv[1];
-    const wchar_t* szcDllPath = argv[2];
+    const wchar_t* szcProcessName = argv[1];
+    const wchar_t* szcDLLPath = argv[2];
 
-    HANDLE hProcess = GetProcessHandle(szcGameName);
-    LPVOID lpProcessSzcDllPath = WriteHackDllToProcess(hProcess, szcDllPath);
-    HANDLE hThread = CreateHackThread(hProcess, lpProcessSzcDllPath);
+    HANDLE hProcess = GetProcessHandle(szcProcessName);
+    LPVOID lpProcessSzcDLLPath = WriteHackDLLToProcess(hProcess, szcDLLPath);
+    HANDLE hThread = CreateHackThread(hProcess, lpProcessSzcDLLPath);
     WaitThread(hThread);
 
     VirtualFreeEx(
         hProcess,
-        lpProcessSzcDllPath,
+        lpProcessSzcDLLPath,
         0,
         MEM_RELEASE);
     CloseHandle(hThread);
